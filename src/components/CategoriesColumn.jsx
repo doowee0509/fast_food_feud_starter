742c3f2d779be36a2f2037376f35d4ee36ed83ae
doowee0509 <ngocduy000937@gmail.com>
2022-06-14 13:53:51 -0400

    almost working close btn feature

--- src/components/CategoriesColumn.jsx
+++ src/components/CategoriesColumn.jsx
@@ -2,17 +2,15 @@ import * as React from "react"
 import Chip from "./Chip/Chip"
 
 export function CategoriesColumn(props) {
-    const [state, setState] = React.useState(true)
-    
     return (
         <div className="CategoriesColumn col">
-            <div className="categories options">
-            <h2 className="title">Categories</h2>
-            {props.categories.map((category) => (
-                <Chip key={category} label={category} onClick={()=> props.setter(category)} reset={()=> setState(true)} close={()=> setState(false)} isActive={props.cat === category && state}/>
-            ))}
-            </div>
+        <div className="categories options">
+        <h2 className="title">Categories</h2>
+        {props.categories.map((category) => (
+            <Chip key={category} label={category} onClick={()=> {props.setter(category); console.log('btn-clicked')}} close={()=> {props.setter(null); console.log('x clicked')}}isActive={props.cat === category}/>
+        ))}
         </div>
+    </div>
     )
 }
 
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false, onClick, close, reset}) {
+export function Chip({ label = "", isActive = false, onClick, close}) {
   var buttonClassName = isActive ? 'chip active' : 'chip'
   return (
     <button className={buttonClassName} onClick={onClick}>
-      <p className="label" onClick={reset}>{label}</p>
+      <p className="label">{label}</p>
       <span className="close" role="button" onClick={close}>{`X`}</span>
     </button>
   )
