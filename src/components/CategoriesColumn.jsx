2708c17825269dfc3ed6b98e6a0e9143a03510b5
doowee0509 <ngocduy000937@gmail.com>
2022-06-14 11:58:42 -0400

    closest attempt to deselect item stretch feature

--- src/components/CategoriesColumn.jsx
+++ src/components/CategoriesColumn.jsx
@@ -2,13 +2,13 @@ import * as React from "react"
 import Chip from "./Chip/Chip"
 
 export function CategoriesColumn(props) {
-    
+    const [state, setState] = React.useState(true)
     return (
         <div className="CategoriesColumn col">
         <div className="categories options">
         <h2 className="title">Categories</h2>
         {props.categories.map((category) => (
-            <Chip key={category} label={category} onClick={()=> props.setter(category)} close={()=> props.setter(null)} isActive={props.cat === category}/>
+            <Chip key={category} label={category} onClick={()=> props.setter(category)} reset={()=> setState(true)} close={()=> setState(false)} isActive={props.cat === category && state}/>
         ))}
         </div>
     </div>
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false, onClick, close}) {
+export function Chip({ label = "", isActive = false, onClick, close, reset}) {
   var buttonClassName = isActive ? 'chip active' : 'chip'
   return (
     <button className={buttonClassName} onClick={onClick}>
-      <p className="label">{label}</p>
+      <p className="label" onClick={reset}>{label}</p>
       <span className="close" role="button" onClick={close}>{`X`}</span>
     </button>
   )
